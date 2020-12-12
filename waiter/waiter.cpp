c9065bd27e0d6c7b4f5c6ba0281e862e02d75fa9
ERJennings <erjennings13@gmail.com>
2020-12-11 23:01:00 -0800

    Finished waiter, incredibly tired and order doesn't sound like a word anymore

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 
 #include "../includes/waiter.h"
+#include "../includes/externs.h"
 
 using namespace std;
 
@@ -14,9 +15,29 @@ Waiter::~Waiter()
 
 //gets next Order(s) from file_IO
 int Waiter::getNext(ORDER &anOrder){
+
+	int nextOrder = myIO.getNext(anOrder);
+
+	//Seems like I could just return nextOrder, but header file implies otherwise
+	if (nextOrder != SUCCESS) {
+		b_WaiterIsFinished = true;
+		return NO_ORDERS;
+	}
+
 	return SUCCESS;
+
 }
 
 void Waiter::beWaiter() {
+
+	ORDER newOrder;
+
+	while(getNext(newOrder) == SUCCESS) {
+		lock_guard<mutex> lock(mutex_order_inQ);
+		order_in_Q.push(newOrder);
+	}
+
+	b_WaiterIsFinished = true;
+	cv_order_inQ.notify_all();
 }
 
